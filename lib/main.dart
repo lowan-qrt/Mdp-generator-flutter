6ff21b9d1ba9f1a36a0a2b0a376b9c2961d963fb
Lowan Quarton <lowan.qrt@gmail.com>
2026-03-03 22:43:25 +0100

    Refactor MyHomePage to StatefulWidget, add password generation options with checkboxes and input field for length. Update UI elements for better organization and clarity.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,17 +20,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  bool _hasLower = true;
+  bool _hasUpper = false;
+  bool _hasNumbers = false;
+  bool _hasSpecial = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
@@ -43,60 +54,114 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: Container(
-                // decoration: BoxDecoration(
-                //   border: Border.all(color: Colors.red, width: 2),
-                //   borderRadius: BorderRadius.circular(8),
-                // ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+              // child:
+              // Container(
+              // decoration: BoxDecoration(                           // visu pour m'aider
+              //   border: Border.all(color: Colors.red, width: 2),
+              //   borderRadius: BorderRadius.circular(8),
+              // ),
+              child: Column(
+                spacing: 60,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Mot de passe généré",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IntrinsicWidth(
+                    // (width: fit-content)
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
                         Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
                           child: Text(
-                            "Mot de passe généré",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
+                            "Options de sécurité",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      spacing: 10,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            print("azAZ".codeUnits);
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                          decoration: InputDecoration(
+                            labelText: "Longueur",
+                            hintText: "Ex: 12",
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (value) {
+                            int length = int.tryParse(value) ?? 1;
+                            print("Nouvelle longueur : $length");
                           },
-                          child: Row(
-                            children: const [
-                              Icon(Icons.auto_awesome),
-                              SizedBox(width: 8),
-                              Text('Générer'),
-                            ],
-                          ),
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Row(
-                            children: const [
-                              Icon(Icons.copy),
-                              SizedBox(width: 8),
-                              Text('Copier'),
-                            ],
-                          ),
+                        OptionCheckBox(
+                          label: 'Minuscules',
+                          value: _hasLower,
+                          onChanged: (val) =>
+                              setState(() => (_hasLower = val!)),
+                        ),
+                        OptionCheckBox(
+                          label: 'Majuscules',
+                          value: _hasUpper,
+                          onChanged: (val) => setState(() => _hasUpper = val!),
+                        ),
+                        OptionCheckBox(
+                          label: 'Chiffres',
+                          value: _hasNumbers,
+                          onChanged: (val) =>
+                              setState(() => _hasNumbers = val!),
+                        ),
+                        OptionCheckBox(
+                          label: 'Caractères spéciaux',
+                          value: _hasSpecial,
+                          onChanged: (val) =>
+                              setState(() => (_hasSpecial = val!)),
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 10,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          print("azAZ".codeUnits);
+                        },
+                        child: Row(
+                          children: const [
+                            Icon(Icons.auto_awesome),
+                            SizedBox(width: 8),
+                            Text('Générer'),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: const [
+                            Icon(Icons.copy),
+                            SizedBox(width: 8),
+                            Text('Copier'),
+                          ],
+                        ),
+                      ),
+                      // MyLabeledCheckbox(label: "Test", value: true, onChanged: (val) {})
+                    ],
+                  ),
+                ],
               ),
+              // ),
             ),
           ],
         ),
@@ -110,6 +175,28 @@ class MyHomePage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class OptionCheckBox extends StatelessWidget {
+  const OptionCheckBox({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.onChanged,
+  });
+
+  final String label;
+  final bool value;
+  final ValueChanged<bool?> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      value: value,
+      title: Text(label),
+      onChanged: onChanged,
     );
   }
 }
