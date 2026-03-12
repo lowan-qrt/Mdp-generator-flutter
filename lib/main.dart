d51731264cc4e4a539cbed18b99eae4e0fcfc0d3
Lowan Quarton <lowan.qrt@gmail.com>
2026-03-12 20:34:47 +0100

    Refactor password generation logic in MyHomePage to improve code clarity and maintainability. Introduce TextEditingController for password length input and enhance random character selection process.

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
@@ -36,10 +35,23 @@ class _MyHomePage extends State<MyHomePage> {
   bool _hasNumbers = false;
   bool _hasSpecial = false;
   int _length = 12;
+  final TextEditingController _lengthController = TextEditingController();
 
   String _generatedPwd = '';
 
-  /// Plages de caractères à utiliser
+  @override
+  void initState() {
+    super.initState();
+    _lengthController.text = _length.toString();
+  }
+
+  @override
+  void dispose() {
+    _lengthController.dispose();
+    super.dispose();
+  }
+
+  // Chars plages
   List<List<int>> getSelectedCharRanges() {
     List<List<int>> ranges = [];
     if (_hasLower) ranges.add([97, 122]); // a-z
@@ -57,28 +69,30 @@ class _MyHomePage extends State<MyHomePage> {
   }
 
   String generatePwd(int nbChars) {
+    // Get selected chars ranges based on user options
     List<List<int>> ranges = getSelectedCharRanges();
     if (ranges.isEmpty) return '';
 
-    Random rnd = Random.secure();
-
+    // Store pwd chars here
     List<int> chars = [];
 
+    // Ensure at least one char from each selected option
     for (final range in ranges) {
-      final codeUnit = range[0] + rnd.nextInt(range[1] - range[0] + 1);
+      final codeUnit = range[0] + Random().nextInt(range[1] - range[0] + 1);
       chars.add(codeUnit);
     }
 
+    // Nb of chars left to fill
     final remaining = nbChars - chars.length;
 
+    // Fill remaining chars randomly from the ranges
     for (int i = 0; i < remaining; ++i) {
-      final range = ranges[rnd.nextInt(ranges.length)];
-      final codeUnit = range[0] + rnd.nextInt(range[1] - range[0] + 1);
+      final range = ranges[Random().nextInt(ranges.length)];
+      final codeUnit = range[0] + Random().nextInt(range[1] - range[0] + 1);
       chars.add(codeUnit);
     }
 
-    chars.shuffle(rnd);
-
+    chars.shuffle();
     return String.fromCharCodes(chars);
   }
 
@@ -101,136 +115,152 @@ class _MyHomePage extends State<MyHomePage> {
               ),
             ),
             Expanded(
-              child: Column(
-                spacing: 60,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
+              // Main middle part
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: SingleChildScrollView(
+                  child: Column(
+                    spacing: 60,
                     mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        _generatedPwd.isNotEmpty ? _generatedPwd : "Mot de passe généré",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            _generatedPwd.isNotEmpty
+                                ? _generatedPwd
+                                : "Mot de passe généré",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      IntrinsicWidth(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: Text(
+                                "Options de sécurité",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              decoration: InputDecoration(
+                                labelText: "Longueur",
+                                hintText: "Ex: 12",
+                                border: OutlineInputBorder(),
+                              ),
+                              controller: _lengthController,
+                              onChanged: (value) {
+                                setState(() {
+                                  _length = int.tryParse(value) ?? 1;
+                                });
+                              },
+                            ),
+                            OptionCheckBox(
+                              label: 'Minuscules',
+                              value: _hasLower,
+                              onChanged: (value) =>
+                                  setState(() => (_hasLower = value!)),
+                            ),
+                            OptionCheckBox(
+                              label: 'Majuscules',
+                              value: _hasUpper,
+                              onChanged: (value) =>
+                                  setState(() => _hasUpper = value!),
+                            ),
+                            OptionCheckBox(
+                              label: 'Chiffres',
+                              value: _hasNumbers,
+                              onChanged: (value) =>
+                                  setState(() => _hasNumbers = value!),
+                            ),
+                            OptionCheckBox(
+                              label: 'Caractères spéciaux',
+                              value: _hasSpecial,
+                              onChanged: (value) =>
+                                  setState(() => (_hasSpecial = value!)),
+                            ),
+                          ],
                         ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 10,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              final ranges = getSelectedCharRanges();
+                              if (_length < ranges.length) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      "Longueur insuffisante pour couvrir tous les types sélectionnés.",
+                                    ),
+                                  ),
+                                );
+                                return;
+                              }
+                              final pwd = generatePwd(_length);
+                              setState(() {
+                                _generatedPwd = pwd;
+                              });
+                            },
+                            child: Row(
+                              children: const [
+                                Icon(Icons.auto_awesome),
+                                SizedBox(width: 8),
+                                Text('Générer'),
+                              ],
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_generatedPwd.isNotEmpty) {
+                                Clipboard.setData(
+                                  ClipboardData(text: _generatedPwd),
+                                );
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      "Mot de passe copié dans le presse-papiers !",
+                                    ),
+                                    duration: Duration(seconds: 2),
+                                  ),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      "Aucun mot de passe à copier.",
+                                    ),
+                                    duration: Duration(seconds: 2),
+                                  ),
+                                );
+                              }
+                            },
+                            child: Row(
+                              children: const [
+                                Icon(Icons.copy),
+                                SizedBox(width: 8),
+                                Text('Copier'),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  IntrinsicWidth(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          child: Text(
-                            "Options de sécurité",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        TextField(
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                          decoration: InputDecoration(
-                            labelText: "Longueur",
-                            hintText: "Ex: 12",
-                            border: OutlineInputBorder(),
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              _length = int.tryParse(value) ?? 1;
-                            });
-                          },
-                        ),
-                        OptionCheckBox(
-                          label: 'Minuscules',
-                          value: _hasLower,
-                          onChanged: (value) =>
-                              setState(() => (_hasLower = value!)),
-                        ),
-                        OptionCheckBox(
-                          label: 'Majuscules',
-                          value: _hasUpper,
-                          onChanged: (value) =>
-                              setState(() => _hasUpper = value!),
-                        ),
-                        OptionCheckBox(
-                          label: 'Chiffres',
-                          value: _hasNumbers,
-                          onChanged: (value) =>
-                              setState(() => _hasNumbers = value!),
-                        ),
-                        OptionCheckBox(
-                          label: 'Caractères spéciaux',
-                          value: _hasSpecial,
-                          onChanged: (value) =>
-                              setState(() => (_hasSpecial = value!)),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          final ranges = getSelectedCharRanges();
-                          if (_length < ranges.length) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(
-                                    "Longueur insuffisante pour couvrir tous les types sélectionnés."),
-                              ),
-                            );
-                            return;
-                          }
-                          final pwd = generatePwd(_length);
-                          setState(() {
-                            _generatedPwd = pwd;
-                          });
-                        },
-                        child: Row(
-                          children: const [
-                            Icon(Icons.auto_awesome),
-                            SizedBox(width: 8),
-                            Text('Générer'),
-                          ],
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_generatedPwd.isNotEmpty) {
-                            Clipboard.setData(ClipboardData(text: _generatedPwd));
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Mot de passe copié dans le presse-papiers !"),
-                                duration: Duration(seconds: 2),
-                              ),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Aucun mot de passe à copier."),
-                                duration: Duration(seconds: 2),
-                              ),
-                            );
-                          }
-                        },
-                        child: Row(
-                          children: const [
-                            Icon(Icons.copy),
-                            SizedBox(width: 8),
-                            Text('Copier'),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
           ],
